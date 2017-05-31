d23e45cd812fa50f96d44eae62955ed1b9b83549
Daniel Espinosa <gangsterdan@gmail.com>
2017-05-31 12:02:44 -0500

    Completar consulta de capacidad

--- hospitals/app/controllers/hospital_controller.rb
+++ hospitals/app/controllers/hospital_controller.rb
@@ -1,4 +1,24 @@
+require 'HTTPClient'
+
 class HospitalController < ApplicationController
   def index
   end
+
+  def procesar
+    id = params[:consulta][:id]
+
+
+
+    client = HTTPClient.new
+    cap = client.get_content('http://130.206.112.201:1026/v2/entities/' + id + '/attrs/capacidad/value')
+
+    if params[:consulta][:accion] == "1"
+      res = cap.to_i + 1
+    else
+      res = cap.to_i - 1
+    end
+
+    puts res
+  end
+
 end
--- hospitals/config/routes.rb
+++ hospitals/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'hospital/index'
+
+  get 'hospital/index' => 'hospital#index'
+  post 'hospital/procesar' => 'hospital#procesar'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
