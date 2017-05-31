addd189c3b7461ac33894ce7bcefaf926a4482e9
Daniel Espinosa <gangsterdan@gmail.com>
2017-05-31 12:50:14 -0500

    Agregar RestClient que permite put, impedir capacidades menores a 0. Actualizaciones correctas

--- hospitals/app/controllers/hospital_controller.rb
+++ hospitals/app/controllers/hospital_controller.rb
@@ -1,4 +1,5 @@
 require 'HTTPClient'
+require 'rest-client'
 
 class HospitalController < ApplicationController
   def index
@@ -7,18 +8,23 @@ class HospitalController < ApplicationController
   def procesar
     id = params[:consulta][:id]
 
-
-
     client = HTTPClient.new
     cap = client.get_content('http://130.206.112.201:1026/v2/entities/' + id + '/attrs/capacidad/value')
+    camas = client.get_content('http://130.206.112.201:1026/v2/entities/' + id + '/attrs/camas/value')
 
     if params[:consulta][:accion] == "1"
-      res = cap.to_i + 1
+      res = cap.to_f + (100.0/camas.to_i)
     else
-      res = cap.to_i - 1
+      res = cap.to_f - (100.0/camas.to_i)
     end
 
-    puts res
+    puts 100.0/camas.to_i
+
+    if res < 0
+      res = 0
+    end
+
+    r = RestClient.put('http://130.206.112.201:1026/v2/entities/' + id + '/attrs/capacidad/value/', res.to_s, content_type: 'text/plain')
   end
 
 end
