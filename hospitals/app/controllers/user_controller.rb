4dbd6fb57873f268d3d145cca3c5d23566e9f51f
Daniel Espinosa <gangsterdan@gmail.com>
2017-06-01 13:25:52 -0500

    Ponderar, elegir mejor hospital, redirigir a google maps con ruta

--- hospitals/app/controllers/user_controller.rb
+++ hospitals/app/controllers/user_controller.rb
@@ -15,22 +15,108 @@ class UserController < ApplicationController
     lat = arrCoords[0]
     long = arrCoords[1]
 
-    compound = lat + ',%20' + long
+    compound = lat + ',' + long
 
-    query = 'http://130.206.112.201:1026/v2/entities?idPattern=.*&type=hospital&georel=near%3BmaxDistance%3A100000&coords=' + compound + '&geometry=point&attrs=capacidad,coordenadas&orderBy=geo%3Adistance&limit=5'
+    #query = 'http://130.206.112.201:1026/v2/entities?idPattern=.*&type=hospital&georel=near%3BmaxDistance%3A100000&coords=' + compound + '&geometry=point&attrs=capacidad,coordenadas&orderBy=geo%3Adistance&limit=4'
+    query = 'http://130.206.112.201:1026/v2/entities?idPattern=.*&type=hospital&georel=near%3BmaxDistance%3A100000&coords=' + compound + '&geometry=point&attrs=capacidad,coordenadas,nombre&orderBy=geo%3Adistance&limit=4'
 
-    queryAmazon = 'http://ataraxia.hopto.org:1026/v2/entities?idPattern=.*&type=hospital&georel=near%3BmaxDistance%3A100000&coords=' + compound + '&geometry=point&attrs=capacidad,coordenadas&orderBy=geo%3Adistance&limit=5'
+    queryAmazon = 'http://ataraxia.hopto.org:1026/v2/entities?idPattern=.*&type=hospital&georel=near%3BmaxDistance%3A100000&coords=' + compound + '&geometry=point&attrs=capacidad,coordenadas&orderBy=geo%3Adistance&limit=4'
 
     client = HTTPClient.new
-    response = client.get_content(queryAmazon)
+    response = client.get_content(query)
 
     puts response
 
     jasons = JSON.parse response
 
-    for i in 0..4 do
+    lista = Array.new
+
+    #htimes = Hash.new
+
+    for i in 0..3 do
       puts jasons[i]["coordenadas"]["value"]
+      lista << jasons[i]["id"]
+      puts lista[i]
     end
+
+
+    #19.435205,-99.1433887
+
+    #Armar las queries
+    queries = Array.new
+    for i in 0..3 do
+      queries << 'https://maps.googleapis.com/maps/api/directions/json?origin=' + compound + '&destination=' + jasons[i]["coordenadas"]["value"] + '&key=AIzaSyCLrYOeMIb8jNP-stoWcN-wrWed7PgDj5k&departure_time=now'
+    end
+
+    #ejecutar las queries
+    qresults = Array.new
+    for i in 0..3 do
+      qresults << client.get_content(queries[i])
+    end
+
+    #parsear jsons
+    rutasData = Array.new
+    for i in 0..3 do
+      d = JSON.parse qresults[i]
+      rutasData << d
+    end
+
+    #obtener los tiempos
+    tiempos = Array.new
+    for i in 0..3 do
+      tiempos << rutasData[i]["routes"][0]["legs"][0]["duration_in_traffic"]["value"]
+    end
+
+    capacidades = Array.new
+    for i in 0..3
+      capacidades << jasons[i]["capacidad"]["value"]
+    end
+
+    #(100-capacidad)*tiempoAtencion+tiempotraslado
+    #tiempoAtencion 25.4 minutos
+
+    ponderaciones = Array.new
+    for i in 0..3
+      p = (100-capacidades[i])*25.4+(tiempos[i]/60)
+      ponderaciones << p
+    end
+
+    idMejorHospital = 0
+    minActual = 100000
+    indice = -1
+
+    for i in 0..3
+      if ponderaciones[i] < minActual
+        minActual = ponderaciones[i]
+        idMejorHospital = jasons[i]["id"]
+        indice = i
+      end
+    end
+
+    puts "Mejor hospital id"
+    puts idMejorHospital
+
+    puts "Nombre del mejor hospital"
+    puts client.get_content('http://130.206.112.201:1026/v2/entities/' + idMejorHospital + '/attrs/nombre/value')
+
+    #Redirigir al url con las coordenadas del mejor hospital
+
+    coordenadasMejorHospital = client.get_content('http://130.206.112.201:1026/v2/entities/' + idMejorHospital + '/attrs/coordenadas/value')
+    coordenadasMejorHospital = coordenadasMejorHospital[1,coordenadasMejorHospital.length].chomp('"')
+
+
+    mapsURL = 'https://www.google.com/maps/dir/?api=1&origin=' + compound + '&destination=' +coordenadasMejorHospital
+
+    redirect_to(mapsURL)
+
+
+
+
+=begin
+https://maps.googleapis.com/maps/api/directions/json?origin=19.344935,-99.199929&destination=19.358603,-99.155045&key=AIzaSyCLrYOeMIb8jNP-stoWcN-wrWed7PgDj5k&departure_time=now
+
+=end
+
 
 
   end
